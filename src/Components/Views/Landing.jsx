c77d70b38e483877792c75751f6cf3dc5fb096e0
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-26 23:45:23 -0300

    landing responsive

--- src/Components/Views/Landing.jsx
+++ src/Components/Views/Landing.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import style from '../CssComponents/Landing.module.css'
-import backgrounVideo from '../../rain.mp4'
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "../CssComponents/Landing.module.css";
 
 const Landing = () => {
   return (
     <div className={`${style.container}`}>
-      <video autoPlay loop muted id='video' className={`${style.video}`}>
-          <source src={backgrounVideo} type='video/mp4'/>
-      </video>
-    <Link to={'/home'} className={`${style.btn}`}> <button className={`${style.btnHome}`}>Go home</button ></Link>
+      <Link to={"/home"} className={`${style.btn}`}>
+       
+        <button className={`${style.btnHome}`}>Go home</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
